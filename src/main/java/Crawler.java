47cc1b40b0175f5c6debee2f4a39c0bafff0dcec
amotte <nonomotte@gmail.com>
2019-02-04 11:22:30 -0500

    hey

--- src/main/java/Crawler.java
+++ src/main/java/Crawler.java
@@ -1,14 +1,17 @@
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
+import com.mongodb.*;
+
+import java.util.Arrays;
 
 public class Crawler {
     public static void main(String[] args){
 
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
-        DB database = mongoClient.getDB("myMongoDb");
-        for ( String a : mongoClient.getDatabaseNames()){
-            System.out.println();
-        }
+        MongoCredential credential = MongoCredential.createCredential("user", "labdd", "password1".toCharArray());
+
+        MongoClientURI uri = new MongoClientURI("mongodb://user:password1@ds247698.mlab.com:47698/labdd");
+        MongoClient mongoClient = new MongoClient(uri);
+        DB database = mongoClient.getDB("labdd");
+
+       System.out.println(database);
 
     }
 }
